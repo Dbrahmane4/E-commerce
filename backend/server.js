038810336939e1a0893e3e1a28d274ab691a7b2e
Dhananjay <dhananjaybrahmane63652@gmail.com>
2021-08-11 15:41:28 +0530

    some more changes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,16 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/api/product/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if(product){
+        res.send(product);
+    }
+    else{
+        res.status(404).send({message: 'Product not found'});
+    }
+})
+
 app.get('/api/product', (req, res) => {
     res.send(data.products)
 })
