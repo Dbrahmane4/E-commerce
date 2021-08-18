5aa47d5c2292e3c2ad8bea8b8c2dd5074e2980c5
Dhananjay <dhananjaybrahmane63652@gmail.com>
2021-08-18 14:55:06 +0530

    signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/DjMart', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
